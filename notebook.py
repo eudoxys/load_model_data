c0202f162725a4b9eb8379e52018575215c3a986
David P. Chassin <david.chassin@me.com>
2026-05-07 12:41:39 -0500

    Initial commit

---
+++ notebook.py
@@ -0,0 +1,225 @@
+import marimo
+
+__generated_with = "0.23.1"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _(os, pd):
+    if os.path.exists("loadcomp.csv"):
+        data = pd.read_csv("loadcomp.csv",index_col=[0,1,2,3,4])
+    else:
+        data = []
+        for _file in sorted(os.listdir("data")):
+            if not _file.startswith("LoadComp_") or not _file.endswith(".xlsx"):
+                continue
+            _spec = os.path.splitext(_file)[0].split("_", 3)
+            _pathname = f"data/{_file}"
+            try:
+                _xlsx = pd.ExcelFile(_pathname,engine='openpyxl', engine_kwargs={'data_only': True, 'read_only': True})
+                print(_file, end="", flush=True)
+                for _sheet in _xlsx.sheet_names:
+                    print(end=".", flush=True)
+                    _data = pd.read_excel(
+                        f"data/{_file}",
+                        sheet_name=_sheet,
+                        engine="openpyxl",
+                        engine_kwargs={"data_only": True, "read_only": True},
+                    )
+                    _data[["RO", "SEASON", "NOMD", "HOUR"]] = (
+                        _spec[1].upper(),
+                        _spec[2].upper(),
+                        1 if len(_spec) > 3 else 0,
+                        int(_sheet.replace("Hour", "")) - 1,
+                    )
+                    _data.rename({"#AREA": "LOADTYPE"}, axis=1, inplace=True)
+                    _data.drop(
+                        [x for x in _data.columns if x.startswith("ID_")],
+                        inplace=True,
+                        axis=1,
+                    )
+                    _data.columns = [x.upper().replace("LC_","") for x in _data.columns]
+                    data.append(
+                        _data.set_index(
+                            ["RO", "LOADTYPE", "SEASON", "NOMD", "HOUR"]
+                        ).round(6)
+                    )
+            except Exception as err:
+                print(f"ERROR [{_file}]: {err}", flush=True)
+            print(flush=True)
+        data = pd.concat(data)
+        data.to_csv("loadcomp.csv", index=True, header=True)
+    return (data,)
+
+
+@app.cell
+def _(data, mo):
+    _options = data.index.get_level_values(0).unique()
+    ro = mo.ui.dropdown(options=_options,label="Region:",value=_options[0])
+    return (ro,)
+
+
+@app.cell
+def _():
+    # _options = sorted(data.loc[[ro.value]].index.get_level_values(1).unique())
+    # loadtype = mo.ui.dropdown(options=_options,label="Load type:",value=_options[0])
+    return
+
+
+@app.cell
+def _(data, mo, ro):
+    _options = data.loc[[ro.value]].index.get_level_values(1).unique()
+    _btype = sorted({x.split("_",1)[0] for x in _options})
+    sector = mo.ui.dropdown(options=_btype,label="City/Type:",value=_btype[0])
+    return (sector,)
+
+
+@app.cell
+def _(data, mo, ro, sector):
+    _options = data.loc[[ro.value]].index.get_level_values(1).unique()
+    _ftype = sorted({x.split("_",1)[1] for x in _options if x.startswith(sector.value+"_")})
+    feeder = mo.ui.dropdown(options=_ftype,label="Sector/Load:",value=_ftype[0])
+    return (feeder,)
+
+
+@app.cell
+def _(data, mo):
+    _options = data.index.get_level_values(2).unique()
+    season = mo.ui.dropdown(options=_options,label="Season:",value=_options[0])
+    return (season,)
+
+
+@app.cell
+def _(mo):
+    nomd = mo.ui.checkbox(label="No motor D",value=False)
+    return (nomd,)
+
+
+@app.cell
+def _(mo):
+    hour = mo.ui.slider(start=0,stop=23,label="Hour:",value=15,show_value=True,debounce=True)
+    return (hour,)
+
+
+@app.cell
+def _(mo):
+    # settings
+    show_data_types = mo.ui.checkbox(label="Show data types")
+    show_column_summaries = mo.ui.checkbox(label="Show column summaries")
+    return show_column_summaries, show_data_types
+
+
+@app.cell
+def _(
+    area_data,
+    area_plot,
+    data,
+    feeder,
+    hour,
+    mo,
+    nomd,
+    pie_data,
+    pie_plot,
+    ro,
+    season,
+    sector,
+    show_column_summaries,
+    show_data_types,
+):
+    _options = {
+        "selection": None,
+        "show_data_types": show_data_types.value,
+        "show_column_summaries": show_column_summaries.value,
+        "show_download": True,
+    }
+    mo.ui.tabs(
+        {
+            "Data": mo.ui.table(data,**_options),
+            "Pie": mo.vstack([
+                mo.hstack([ro,sector,feeder,season,hour,nomd],justify='start'),
+                mo.hstack([pie_plot, mo.ui.table(pie_data, **_options)])]),
+            "Area": mo.vstack([
+                mo.hstack([ro,sector,feeder,season,hour,nomd],justify='start'),
+                area_plot, 
+                mo.ui.table(area_data, **_options)]),
+            "Settings": 
+                mo.vstack([
+                    mo.md("**Tables**"),
+                    show_data_types,
+                    show_column_summaries
+                ]),
+        },
+        lazy=True,
+    )
+    return
+
+
+@app.cell
+def _(data, feeder, hour, nomd, np, plt, ro, season, sector):
+    _data = data.loc[
+        np.s_[
+            ro.value,
+            "_".join([sector.value, feeder.value]),
+            season.value,
+            1 if nomd.value else 0,
+            :,
+        ],
+        ["MOTORA", "MOTORB", "MOTORC", "MOTORD", "PWRELEC", "ZP", "IP"],
+    ]
+    area_data = (
+        _data.reset_index()
+        .set_index("HOUR")
+        .drop(["RO", "LOADTYPE", "SEASON", "NOMD"], axis=1)
+    )
+    area_plot = area_data.plot.area(
+        figsize=(15, 5),
+        grid=True,
+        xlim=[0,23],
+        ylabel="Fraction of load",
+        title=f"{ro.value} {feeder.value} {sector.value} {season.value} {'NO ' if nomd.value else 'W/'}MOTOR D",
+    )
+    area_plot.plot([hour.value,hour.value],[0,1],linewidth=3,color="k");
+    plt.legend(loc="right");
+    return area_data, area_plot
+
+
+@app.cell
+def _(data, feeder, hour, nomd, ro, season, sector):
+    _data = data.loc[
+        [
+            (
+                ro.value,
+            "_".join([sector.value,feeder.value]),
+                season.value,
+                1 if nomd.value else 0,
+                hour.value,
+            )
+        ],
+        ["MOTORA", "MOTORB", "MOTORC", "MOTORD", "PWRELEC", "ZP", "IP"],
+    ]
+    _data = (
+        _data.stack()
+        .reset_index()
+        .rename({"level_5": "ENDUSE", 0: "VALUE"}, axis=1)
+    )
+    pie_data = _data.set_index("ENDUSE").drop(["RO","LOADTYPE","SEASON","HOUR","NOMD"],axis=1)
+    pie_plot = pie_data.plot(
+        kind="pie", y="VALUE", x=pie_data.index, legend=False, autopct="%.1f%%", title=f"{ro.value} {feeder.value} {sector.value} {season.value} {hour.value}h {'NO ' if nomd.value else 'W/'}MOTOR D"
+    )
+    return pie_data, pie_plot
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import os
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    pd.options.display.width=None
+    pd.options.display.max_columns=None
+    return mo, np, os, pd, plt
+
+
+if __name__ == "__main__":
+    app.run()
